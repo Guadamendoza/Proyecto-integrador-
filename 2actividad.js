8f013b8e1519ad03966caa3958017fb60a8e796f
Guadamendoza <despontinguadalupe@gmail.com>
2025-04-18 14:54:31 -0300

    Add files via upload

---
+++ 2actividad.js
@@ -0,0 +1,9 @@
+const persona = {
+    nombre: "Lucía",
+    edad: 28,
+    profesion: "Diseñadora",
+   };
+    const {nombre, edad, profesion} = persona;
+    console.log(`${nombre} tiene ${edad} años y trabaja como ${profesion}.`);
+
+    persona.ciudad = "Rosario";
