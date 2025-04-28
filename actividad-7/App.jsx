8f9e12f3204b423d1960d90b6b7b720035ddd458
Guadamendoza <despontinguadalupe@gmail.com>
2025-04-28 19:49:19 -0300

    Add files via upload

---
+++ actividad-7/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+// components
+import Contact from './componentes/contact.jsx'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Taildwin</h1>
+      <Contact></Contact>
+      </>
+        
+  )
+}
+
+export default App
