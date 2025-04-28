6a088356fdaab17782b639318734180dbb33511d
Guadamendoza <despontinguadalupe@gmail.com>
2025-04-28 10:58:51 -0300

    Add files via upload

---
+++ actividad-4/Presentacion.jsx
@@ -0,0 +1,10 @@
+export default function Presentacion(props) {
+    return (
+      <div>
+        <p>Nombre: {props.nombre}</p>
+        <p>Edad: {props.edad}</p>
+        <p>Profesión: {props.profesion}</p>
+      </div>
+    );
+  }
+  
---
+++ actividad-4/Saludo.jsx
@@ -0,0 +1,6 @@
+function Saludo(props) {
+    return <h2>Hola, {props.nombre}!</h2>;
+  }
+  
+  export default Saludo;
+  
